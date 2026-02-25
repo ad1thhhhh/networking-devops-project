fb5ef5262cc9dd033aec6ae95c1f3c1bf546648e
Adith Ravi <adithravi23.ie@gmail.com>
2026-02-25 12:43:23 +0000

    Fixed SSH key path for CI

--- terraform/main.tf
+++ terraform/main.tf
@@ -102,7 +102,7 @@ resource "azurerm_linux_virtual_machine" "networking_vm" {
 
   admin_ssh_key {
     username   = "adithravi"
-    public_key = file("~/.ssh/id_rsa.pub")
+    public_key = file("${path.module}/id_rsa.pub")
   }
 
   os_disk {
