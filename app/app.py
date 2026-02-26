d319756721d258fe8a540d16e4e650e0d6874570
Adith Ravi <adithravi23.ie@gmail.com>
2026-02-26 14:46:50 +0000

    memory usage addition

--- app/app.py
+++ app/app.py
@@ -7,6 +7,7 @@ app = Flask(__name__)
 def cpu():
     return {
         "cpu_usage_percent": psutil.cpu_percent(interval=1)
+        "memory_usage_percent": psutil.virtual_memory().percent
     }
 
 @app.route("/")
