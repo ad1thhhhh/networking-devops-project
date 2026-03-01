dd14f8b43a0c33eaec5bdb5b900b2a6909e8845e
Adith Ravi <adithravi23.ie@gmail.com>
2026-03-01 21:10:07 +0000

    test run

--- app/app.py
+++ app/app.py
@@ -1,34 +1,21 @@
+#Test Run
+
 from flask import Flask, render_template
 
 app = Flask(__name__)
 
-# ---------------------------
-# Login Page
-# ---------------------------
 @app.route("/")
 def login():
     return render_template("login.html")
 
-
-# ---------------------------
-# Artisan Dashboard
-# ---------------------------
 @app.route("/artisan")
 def artisan():
     return render_template("artisan.html")
 
-
-# ---------------------------
-# Curator Dashboard
-# ---------------------------
 @app.route("/curator")
 def curator():
     return render_template("curator.html")
 
-
-# ---------------------------
-# Run Server
-# ---------------------------
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000, debug=True)
